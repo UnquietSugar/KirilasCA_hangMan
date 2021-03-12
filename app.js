cd9bc81ceef22c0c28ba0ba3936059a4b0fe8c5d
KIiril Konstandi <kirilask@gmail.com>
2021-03-12 14:47:16 +0200

    second

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+let letter = document.getElementsByClassName('letter')
+let hangMan = document.getElementsByClassName('hangMan')
+let input = document.getElementById('input')
+
+const word = 'labas'
+
+input.addEventListener('keypress',check)
+
+function check(){
+
+}
