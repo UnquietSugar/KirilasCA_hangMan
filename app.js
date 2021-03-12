15106ee38e22dda1f1d5660233cbdadc6e765b22
KIiril Konstandi <kirilask@gmail.com>
2021-03-12 15:00:03 +0200

    third

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ let letter = document.getElementsByClassName('letter')
 let hangMan = document.getElementsByClassName('hangMan')
 let input = document.getElementById('input')
 
-const word = 'labas'
+
 
 input.addEventListener('keypress',check)
 
-function check(){
 
+
+function check(){
+    let counter = 0
+
+
+    for (let i = 0; i < letter.length; i++) {
+        if (letter[i].innerText.toUpperCase() === input.value.toUpperCase()){
+            letter[i].style.color='black'
+        }
+    }
+    cleanInput()
+}
+
+
+function cleanInput(){
+    input.value=''
 }
