3ffba33413013b3f5e912a79efe38a0170e16886
KIiril Konstandi <kirilask@gmail.com>
2021-03-12 15:52:59 +0200

    fourth

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 let letter = document.getElementsByClassName('letter')
-let hangMan = document.getElementsByClassName('hangMan')
-let input = document.getElementById('input')
+const hangMan = document.getElementsByClassName('hangMan')
+const input = document.getElementById('input')
+const lines = document.getElementById('lines')
+const newGame = document.querySelector('.newGame')
 
-
-
+let words =['car','shop','apple','house','horse','']
+let letterArr=[]
+newGame.addEventListener('click',ng)
 input.addEventListener('keypress',check)
 
 
 
-function check(){
-    let counter = 0
 
 
-    for (let i = 0; i < letter.length; i++) {
-        if (letter[i].innerText.toUpperCase() === input.value.toUpperCase()){
-            letter[i].style.color='black'
+function ng(){
+    letterArr=[]
+    let word = words[Math.floor(Math.random()*words.length)]
+    for (let i = 0; i < word.length; i++) {
+        console.log(word)
+        lines.innerHTML+=`<div class="letter">${word[i]}</div>`
+        letterArr.push(word[i])
+    }
+}
+
+
+
+
+function check(e){
+    if (e.key === 'Enter'){
+        let counter = Number(JSON.parse(localStorage.getItem('counter')))
+
+        for (let i = 0; i < letterArr.length; i++) {
+            if (letterArr[i].toUpperCase() === input.value.toUpperCase()){
+                letter[i].style.color='black'
+                letterArr.splice(i,1)
+            }
         }
     }
-    cleanInput()
+
 }
 
 
